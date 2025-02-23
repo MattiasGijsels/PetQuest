823bb5da0b0d0f59fc3f59082e669b4b93b976b3
Mattias Gijsels <mattias.gijsels@hotmail.com>
2025-02-23 16:29:09 +0100

    added parts of the contracts

---
+++ PetQuest.Contracts/Models/Pet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using PetQuest.Contracts.Shared;
+using PetQuest.Contracts.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetQuest.Contracts.Models
+{
+    public class Pet : ModelBase
+    {
+        [Required]
+        [StringLength(100)]
+        public string PetName { get; set; } = default!;
+
+        [Required]
+        [StringLength(50)]
+        public  Species Name { get; set; } = new Species();
+
+        [Required]
+        [StringLength(50)]
+        public string Breed { get; set; } = default!;
+
+        public int Age { get; set; }
+        
+        
+
+
+        public Pet() : base() { }
+
+
+
+    }
+}
---
+++ PetQuest.Contracts/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetQuest.Contracts.Models
+{
+    internal class User
+    {
+    }
+}
